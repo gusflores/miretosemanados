eaef529cc7c2979209dd5b1da23bae79e9cea6e5
Jesús Gustavo Flores Eraña <gustavo.flores.erana@gmail.com>
2015-11-13 19:20:05 -0600

    Initial Commit

---
+++ Minireto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Minireto
+//
+//  Created by Jesús Gustavo Flores Eraña on 13/11/15.
+//  Copyright © 2015 Jesús Gustavo Flores Eraña. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
